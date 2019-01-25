db24b20154fff7ebf7db48b517483ceb06ef13bb
DavPen2001 <david_rodriguez@davincischools.org>
2019-01-25 15:33:26 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// if you want to change something on a DIFFERENT element, you have to call it by name (it's "id")
+// https://www.w3schools.com/jsref/met_document_getelementbyid.asp
+
+// "el" stands for the "element" that was just clicked
+function allOpaque(el){
+	document.getElementById("opaque-pats").classList.add("opaque");
+	document.getElementById("opaque-rams").classList.add("opaque");
+
+}
+
+// "el" stands for the "element" that was just clicked
+function removeColor(el){
+	document.getElementById("pats-column").classList.remove("pats-silver");
+	document.getElementById("rams-column").classList.remove("rams-blue");
+}
+
+function showTrophy(el){
+	document.getElementById("opaque-rams").src = "images/lombardi_trophy.png";
+	document.getElementById("mid").src = "images/lombardi_trophy.png";
+	document.getElementById("opaque-pats").src = "images/lombardi_trophy.png";
+}
